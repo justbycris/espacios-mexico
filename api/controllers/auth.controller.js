56ad849537e76a9fab2659617b16e7ab41f6351d
Cristina Gutierrez <cristigtzname@gmail.com>
2024-08-27 21:14:54 -0400

    completed signup page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,10 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-import { errorHandler } from '../utils/error.js'
 
-export const signup = async(req, res, next ) => {
-   const { username, email, password } = req.body;
+
+
+export const signup = async (req, res, next ) => {
+   const { username, email, password } = await req.body;
    const hashedPassword = bcryptjs.hashSync(password, 10);
    const newUser = new User({ username, email, password: hashedPassword});
    try {
@@ -11,6 +12,7 @@ export const signup = async(req, res, next ) => {
     res.status(201).json("User created succesfully!");
    } catch (error) {
     next(error);
+    console.log(error)
    }
    
 }
